b372975bdc76077d75c9d7c642a7710e109cf7e1
jkakos <jameskakos491@gmail.com>
2026-06-01 16:20:01 -0700

    Added type hints and returned scores rather than print

--- src/scoring.py
+++ src/scoring.py
@@ -1,4 +1,11 @@
+from typing import Any, cast, Protocol
+
 import matplotlib.pyplot as plt
+import numpy.typing as npt
+import pandas as pd
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from sklearn.base import BaseEstimator
 from sklearn.metrics import (
     classification_report,
     ConfusionMatrixDisplay,
@@ -7,29 +14,62 @@ from sklearn.metrics import (
 )
 
 
-def print_scores(clf, X_test, y_test):
+class Classifier(Protocol):
     """
-    Print classification report and ROC-AUC score for a given classifier.
+    General structural type needed to score a classifier.
+
+    """
+
+    def predict(self, X: pd.DataFrame) -> npt.NDArray: ...
+
+    def predict_proba(self, X: pd.DataFrame) -> npt.NDArray: ...
+
+
+def calc_scores(
+    clf: Classifier, X_test: pd.DataFrame, y_test: pd.Series
+) -> dict[str, float]:
+    """
+    Calculate the classification report and ROC-AUC score for a
+    given classifier.
 
     """
     y_pred = clf.predict(X_test)
     y_prob = clf.predict_proba(X_test)[:, 1]
+    auc_score = roc_auc_score(y_test, y_prob)
 
-    print('Classification Report')
-    print(classification_report(y_test, y_pred))
-    print(f"ROC-AUC Score: {roc_auc_score(y_test, y_prob):.4f}")
+    report_dict = cast(
+        dict[str, Any], classification_report(y_test, y_pred, output_dict=True)
+    )
+    report = report_dict['weighted avg']
+    report['accuracy'] = report_dict['accuracy']
+    report['AUC'] = auc_score
+
+    return report
 
 
-def plot_confusion_matrix_roc_auc(clf, X_test, y_test):
+def plot_confusion_matrix_roc_auc(
+    clf: BaseEstimator, X_test: pd.DataFrame, y_test: npt.NDArray | pd.Series
+) -> tuple[Figure, list[Axes]]:
     """
     Plot a confusion matrix and ROC curve for a given classifier.
 
     """
-    _, ax = plt.subplots(figsize=(10, 4), ncols=2, constrained_layout=True)
+    fig, ax = plt.subplots(figsize=(8, 3.5), ncols=2, constrained_layout=True)
 
-    ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test, cmap='turbo', ax=ax[0])
+    disp = ConfusionMatrixDisplay.from_estimator(
+        clf, X_test, y_test, cmap='turbo', ax=ax[0]
+    )
     RocCurveDisplay.from_estimator(clf, X_test, y_test, ax=ax[1])
+
+    if disp.text_ is not None:
+        for text in disp.text_.ravel():
+            count = int(float(text.get_text()))
+            percentage = (count / len(y_test)) * 100
+            text.set_text(f"{count:,}\n({percentage:.1f}%)")
+            text.set_color('k')
+            text.set_bbox(dict(facecolor='white', alpha=0.5, edgecolor='none'))
 
     ax[0].set_title('Confusion Matrix')
     ax[1].set_title('ROC Curve')
-    plt.show()
+
+    return fig, list(ax)

cd1dce451ddc36e7ed28950584e68d9063b44f23
jkakos <jameskakos491@gmail.com>
2026-06-01 16:23:56 -0700

    Centralized data loading info and simplified query template generation

--- src/config.py
+++ src/config.py
@@ -1,3 +1,6 @@
+from enum import Enum
+import pandas as pd
+
 PROJECT_ID = 'project-e18491ab-d50e-4c36-a7d'
 
 DTYPE_MAP = {
@@ -65,3 +68,88 @@ def set_dtypes(df: pd.DataFrame) -> pd.DataFrame:
         col: dtype for (col, dtype) in DTYPE_MAP.items() if col in df.columns
     }
     return df.astype(active_dtypes)
+
+
+class ProjectStage(Enum):
+    """
+    Create a general structure for project stages.
+
+    """
+
+    RAW = 'raw'
+    CLEANING = 'cleaning'
+    FEATURE_ENG = 'feature_eng'
+
+
+class DatasetSplit(Enum):
+    """
+    List the current available options for dataset splits.
+
+    """
+
+    TRAIN = 'train'
+    TEST = 'test'
+
+
+DATASET_LOCATION = 'airline_data'  # location in BigQuery of all the data/views
+SQL_TEMPLATES = {
+    ProjectStage.CLEANING: '01_initial_cleaning_template.sql.j2',
+    ProjectStage.FEATURE_ENG: '02_features_v{version}_template.sql.j2',
+}
+SQL_TABLE_NAMES = {
+    ProjectStage.RAW: '{dataset}_raw',
+    ProjectStage.CLEANING: '{dataset}_cleaned',
+    ProjectStage.FEATURE_ENG: '{dataset}_features_v{version}',
+}
+SQL_TEMPLATE_VERSIONS = {
+    ProjectStage.CLEANING: 1,
+    ProjectStage.FEATURE_ENG: 4,
+}
+DATASET_SPLITS = {
+    ProjectStage.RAW: [DatasetSplit.TRAIN.value, DatasetSplit.TEST.value],
+    ProjectStage.CLEANING: [DatasetSplit.TRAIN.value, DatasetSplit.TEST.value],
+    ProjectStage.FEATURE_ENG: [DatasetSplit.TRAIN.value, DatasetSplit.TEST.value],
+}
+
+
+def build_table_name(
+    stage: ProjectStage, dataset: DatasetSplit, version: int = 1
+) -> str:
+    """
+    Build the BigQuery view name for a given project stage and dataset split.
+
+    """
+    template = SQL_TABLE_NAMES[stage]
+    view_name = template.format(dataset=dataset.value, version=version)
+    return f'{DATASET_LOCATION}.{view_name}'
+
+
+class ModelVersion(Enum):
+    """
+    Store model versions and the corresponding information regarding the
+    data they will use.
+
+    """
+
+    V1_0 = ('v1.0', ProjectStage.FEATURE_ENG, 1)
+    V1_1 = ('v1.1', ProjectStage.FEATURE_ENG, 1)
+    V2_0 = ('v2.0', ProjectStage.FEATURE_ENG, 2)
+    V2_1 = ('v2.1', ProjectStage.FEATURE_ENG, 3)
+    V3_0 = ('v3.0', ProjectStage.FEATURE_ENG, 4)
+
+    def __init__(
+        self,
+        version_str: str,
+        stage: ProjectStage,
+        sql_version: int,
+    ) -> None:
+        self.version_str = version_str
+        self.stage = stage
+        self.sql_version = sql_version
+
+    def get_table_name(self, dataset: DatasetSplit) -> str:
+        """
+        Get the BigQuery view name for this model version and a given dataset.
+
+        """
+        return build_table_name(self.stage, dataset, version=self.sql_version)
--- src/query_executor.py
+++ src/query_executor.py
@@ -1,79 +1,12 @@
 import pathlib
-from typing import Protocol, Sequence
+from itertools import chain
+from functools import partial
+from typing import Iterator, Protocol, Sequence
 
 from google.cloud import bigquery
 from jinja2 import Environment, FileSystemLoader
 
 import config
-
-
-class TemplateConfig:
-    """
-    A simple class to hold necessary information for generating a SQL query
-    from a Jinja2 template.
-
-    """
-
-    def __init__(self, template_name: str, view_name: str, source_table: str) -> None:
-        self.template_name = template_name
-        self.view_name = view_name
-        self.source_table = source_table
-
-
-class TemplateConfigFactory:
-    """
-    Factory for creating TemplateConfig objects based on combinations of
-    the datasets used and number of versions. The main function
-    get_template_info() returns a list of TemplateConfig objects that can be
-    used by TemplatedViewQuery to generate SQL queries.
-
-    """
-
-    def __init__(
-        self,
-        datasets: Sequence[str],
-        versions: int,
-        template_name: str,
-        view_name: str,
-        source_table: str,
-    ) -> None:
-        self.template_info_list: list[TemplateConfig] | None = None
-        self.datasets = datasets
-        self.versions = versions
-        self.template_name = template_name
-        self.view_name = view_name
-        self.source_table = source_table
-
-    def get_template_info(self) -> list[TemplateConfig]:
-        """
-        Return a list of TemplateConfig objects. If the list has not yet
-        been created, it will be created first and then returned.
-
-        """
-        if self.template_info_list is None:
-            self.template_info_list = self._make_template_info_list()
-
-        return self.template_info_list
-
-    def _make_template_info_list(self) -> list[TemplateConfig]:
-        """
-        Construct a list of TemplateConfig objects.
-
-        """
-        self.template_info_list = []
-        for dataset in self.datasets:
-            for version in range(1, self.versions + 1):
-                self.template_info_list.append(
-                    TemplateConfig(
-                        template_name=self.template_name.format(version=version),
-                        view_name=self.view_name.format(
-                            dataset=dataset, version=version
-                        ),
-                        source_table=self.source_table.format(dataset=dataset),
-                    )
-                )
-
-        return self.template_info_list
 
 
 class SQLQueryProvider(Protocol):
@@ -124,6 +57,47 @@ class TemplatedViewQuery(SQLQueryProvider):
         return f'Executing: {template_parent}/{template_name} -> view: {self.view_name}'
 
 
+def generate_templates(
+    env: Environment,
+    datasets: Sequence[str],
+    versions: int,
+    template: pathlib.Path,
+    view_name: str,
+    source_table: str,
+    dataset_location: str,
+) -> Iterator[TemplatedViewQuery]:
+    """
+    Generator function that yields TemplatedViewQuery objects by iterating
+    through datasets and versions.
+
+    """
+    if versions > 1:
+        t_check = template.with_name(template.name.format(version=1)) == template
+        v_check = view_name.format(dataset=datasets[0], version=1) == view_name.format(
+            dataset=datasets[0], version=2
+        )
+
+        if t_check or v_check:
+            raise ValueError(
+                f"Multiple versions were given ({versions}), but at least one of"
+                " `template` or `view_name` was not version formatted:\n"
+                f'\t template: {template.name}\n\tview_name: {view_name}'
+            )
+
+    for dataset in datasets:
+        for version in range(1, versions + 1):
+            template_fmt = template.with_name(template.name.format(version=version))
+            view_name_fmt = view_name.format(dataset=dataset, version=version)
+            source_table_fmt = source_table.format(dataset=dataset)
+
+            yield TemplatedViewQuery(
+                env=env,
+                template=template_fmt,
+                view_name=f'{dataset_location}.{view_name_fmt}',
+                source_table=f'{dataset_location}.{source_table_fmt}',
+            )
+
+
 def execute_queries(
     query_providers: Sequence[SQLQueryProvider], verbose: bool = True
 ) -> None:
@@ -146,32 +120,33 @@ def execute_queries(
 if __name__ == '__main__':
     SQL_DIR = pathlib.Path(__file__).parents[1].joinpath('sql')
     JINJA_ENV = Environment(loader=FileSystemLoader(SQL_DIR))
-    TEMPLATE_CONFIGS = [
-        TemplateConfigFactory(
-            datasets=['train', 'test'],
-            versions=1,
-            template_name='01_initial_cleaning_template.sql.j2',
-            view_name='{dataset}_cleaned',
-            source_table='{dataset}_raw',
+    STAGES = config.ProjectStage
+    TEMPLATES = config.SQL_TEMPLATES
+    TABLE_NAMES = config.SQL_TABLE_NAMES
+    VERSIONS = config.SQL_TEMPLATE_VERSIONS
+    SPLITS = config.DATASET_SPLITS
+
+    make_templates = partial(
+        generate_templates, env=JINJA_ENV, dataset_location=config.DATASET_LOCATION
+    )
+
+    # Generate the SQL queries used to make new views in BigQuery
+    view_generators = [
+        make_templates(
+            datasets=SPLITS[STAGES.CLEANING],
+            versions=VERSIONS[STAGES.CLEANING],
+            template=SQL_DIR.joinpath(TEMPLATES[STAGES.CLEANING]),
+            view_name=TABLE_NAMES[STAGES.CLEANING],
+            source_table=TABLE_NAMES[STAGES.RAW],
         ),
-        TemplateConfigFactory(
-            datasets=['train', 'test'],
-            versions=2,
-            template_name='02_features_v{version}_template.sql.j2',
-            view_name='{dataset}_features_v{version}',
-            source_table='{dataset}_cleaned',
+        make_templates(
+            datasets=SPLITS[STAGES.FEATURE_ENG],
+            versions=VERSIONS[STAGES.FEATURE_ENG],
+            template=SQL_DIR.joinpath(TEMPLATES[STAGES.FEATURE_ENG]),
+            view_name=TABLE_NAMES[STAGES.FEATURE_ENG],
+            source_table=TABLE_NAMES[STAGES.CLEANING],
         ),
     ]
 
-    view_templates = [
-        TemplatedViewQuery(
-            env=JINJA_ENV,
-            template=SQL_DIR.joinpath(info.template_name),
-            view_name=f'airline_data.{info.view_name}',
-            source_table=f'airline_data.{info.source_table}',
-        )
-        for config in TEMPLATE_CONFIGS
-        for info in config.get_template_info()
-    ]
-
+    view_templates = list(chain.from_iterable(view_generators))
     execute_queries(view_templates)

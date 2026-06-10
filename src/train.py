7f18c9022a7657dc43f22bce22615f84f875ab52
jkakos <jameskakos491@gmail.com>
2026-06-10 08:31:17 -0700

    Renamed pipelines to a more apt notebook_utils

--- src/train.py
+++ src/train.py
@@ -1,6 +1,6 @@
 import argparse
 from typing import Any
-from src import io, model_config, model, pipelines
+from src import io, model_config, model, notebook_utils
 from src.db_config import DatasetSplit
 
 
@@ -14,12 +14,12 @@ def main(model_version: model_config.ModelVersion, hyperparams: dict[str, Any]) 
     data = io.load_data(model_version.get_table_name(DatasetSplit.TRAIN))
 
     # Get features and target
-    features = pipelines.get_model_features(model_version.feature_profile)
+    features = notebook_utils.get_model_features(model_version.feature_profile)
     X = data[features['all']]
     y = data[model_config.TARGET]
 
     # Set up preprocessor and train model
-    preprocessor = pipelines.get_pipeline_preprocessor(features)
+    preprocessor = notebook_utils.get_pipeline_preprocessor(features)
     clf = model.model_from_hyperparams(hyperparams, preprocessor, X, y)
 
     # Save model
@@ -29,10 +29,10 @@ def main(model_version: model_config.ModelVersion, hyperparams: dict[str, Any]) 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train a specific model version.')
     parser.add_argument(
-        "--version",
+        '--version',
         type=str,
         required=True,
-        help="The ModelVersion enum key to run (e.g., V1_0, V1_1, V2_0, etc.)",
+        help='The ModelVersion enum key to run (e.g., V1_0, V1_1, V2_0, etc.)',
     )
     args = parser.parse_args()
     version = io.parse_version_arg(args.version)
